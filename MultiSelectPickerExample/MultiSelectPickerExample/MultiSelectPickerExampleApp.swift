444d22aece25c7a4f9bee640e62ffb95c001167a
Jimoh Yusuf <yusuphjoluwaseun@gmail.com>
2025-03-11 16:57:28 +0000

    created an example test project

---
+++ MultiSelectPickerExample/MultiSelectPickerExample/MultiSelectPickerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiSelectPickerExampleApp.swift
+//  MultiSelectPickerExample
+//
+//  Created by Guru King on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiSelectPickerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
